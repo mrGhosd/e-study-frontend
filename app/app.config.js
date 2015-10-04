00c774ba0166853412ccdd4418c3a5bd1c33cb4e
vsokoltsov <vforvad@gmail.com>
2015-10-04 21:03:26 +0300

    begin implementing of i18n

--- app/app.config.js
+++ app/app.config.js
@@ -1,9 +1,18 @@
-routing.$inject = ['$urlRouterProvider', '$locationProvider'];
+routing.$inject = ['$urlRouterProvider', '$locationProvider', '$translateProvider'];
 
-export default function routing($urlRouterProvider, $locationProvider) {
+export default function routing($urlRouterProvider, $locationProvider, $translateProvider) {
     $locationProvider.html5Mode({
         enabled: true,
         requireBase: false
     });
     $urlRouterProvider.otherwise('/');
+
+    $translateProvider.translations('en', {
+        auth: {
+            "sign_in": "Sign in",
+            "sign_up": "Sign_up",
+            "sign_out": "Sign out"
+        }
+    });
+    $translateProvider.preferredLanguage('en');
 }
--- app/app.js
+++ app/app.js
@@ -2,14 +2,17 @@ import angular from 'angular';
 import uirouter from 'angular-ui-router';
 import angularTranslate from 'angular-translate';
 
-import routes from 'app.config';
+import config from 'app.config';
 import home from 'home/index';
 import users from 'users/main'
 
+import I18n from 'i18n-js';
 import NavigationController from './navigation/NavigationController'
+import HeaderController from './application/HeaderController'
 import './index.html';
 import 'css/main.scss';
 
-console.log(angularTranslate);
-angular.module('estudy', [uirouter, angularTranslate, home, users]).controller('NavigationController', NavigationController)
-    .config(routes);
+angular.module('estudy', [uirouter, angularTranslate, home, users])
+    .controller('NavigationController', NavigationController)
+    .controller('HeaderController', HeaderController)
+    .config(config);
