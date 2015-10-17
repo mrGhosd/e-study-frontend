597c17a877541e8041785994bc442e5d83f49dc7
vsokoltsov <vforvad@gmail.com>
2015-10-17 17:20:03 +0300

    implemented correct output of users list

--- app/users/user.model.js
+++ app/users/user.model.js
@@ -9,6 +9,17 @@ export default class User{
         }
     }
 
+    correctNaming(){
+        if(this.surname && this.name){
+            if(this.secondname){
+                return this.surname + " " + this.name + " " + this.secondname;
+            }
+            return this.surname + " " + this.name;
+        } else {
+            return this.email;
+        }
+    };
+
     humanizedDate(date){
         let newDate = new Date(date);
         let timeSeparated = [newDate.getHours(), newDate.getMinutes(), newDate.getSeconds()];
@@ -20,5 +31,9 @@ export default class User{
         let newDate = new Date(date);
         let dateSeparated = [newDate.getDate(), newDate.getMonth() + 1, newDate.getFullYear()];
         return dateSeparated.join(".");
+    }
+
+    avatarURL(){
+        return this.image ? `http://localhost:3000${this.image.file.url}` : '/images/empty-user.png';
     }
 }
