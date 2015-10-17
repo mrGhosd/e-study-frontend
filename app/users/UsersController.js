6c077cdd27934442050cbf5e21a2028d04865901
vsokoltsov <vforvad@gmail.com>
2015-10-17 16:30:54 +0300

    implement some kind of output of students

--- app/users/UsersController.js
+++ app/users/UsersController.js
@@ -1,11 +1,13 @@
 import UsersService from 'users/users.service';
 
 export default class UsersController {
-    constructor($http, $q){
+    constructor($scope, $http, $q){
+        console.log($scope);
+        this.users = [];
         this.userFactory = new UsersService($http, $q);
         this.userFactory.getAll()
             .then((response) => {
-                console.log(response);
+                $scope.users = this.users = response;
             });
     }
 }
