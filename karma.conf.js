cb9254dab01e520683d5ac6e5a95a63f59523748
vsokoltsov <vforvad@gmail.com>
2015-11-21 19:52:58 +0300

    Finally, implement karma config and specs

--- karma.conf.js
+++ karma.conf.js
@@ -4,53 +4,30 @@ var webpackConfig = require('./config/webpack.config');
 webpackConfig.entry = {};
 module.exports = function(config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: './',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
-
-    // list of files / patterns to load in the browser
     files: [
       'node_modules/angular/angular.js',
-      'node_modules/angular-ui-router/build/angular-ui-router.js',
       'node_modules/angular-mocks/angular-mocks.js',
       'app/app.js',
-      'app/**/*.js',
       'specs/**/*.js'
     ],
 
-
-    // list of files to exclude
     exclude: [
         'app/home/*.js',
-        'app/app.config.js',
         'app/bundle.js',
         'app/util/*.js'
     ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'node_modules/angular': ['webpack'],
+      'app/app.js': ['webpack'],
       'specs/**/*.spec.js': ['webpack'],
-      '**/*.html': ['ng-html2js'],
-      '**/*.js': ['webpack', 'coverage']
+      '**/*.html': ['ng-html2js']
     },
 
     ngHtml2JsPreprocessor: {
 
     },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: [
       'progress',
       'coverage'
@@ -60,13 +37,9 @@ module.exports = function(config) {
       type: 'html',
       dir: 'coverage'
     },
-
     webpackMiddleware: {
-      // webpack-dev-middleware configuration
-      // i. e.
       noInfo: true
     },
-
     plugins: [
       require('karma-webpack'),
       require('karma-jasmine'),
@@ -75,45 +48,24 @@ module.exports = function(config) {
       require('karma-coverage'),
       'ng-html2js'
     ],
-
-    // web server port
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
 
-    webpack: {
-      devtool: 'inline-source-map',
-      modulesDirectories: ['web_modules', 'node_modules', '../app'],
-      module: {
-        loaders: [
-          { test: /\.js?$/, loaders: ['babel-loader?experimental' ], exclude: /node_modules/},
-          { test: /\.html$/, loader: "raw"}
-        ]
-      }
-    },
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
+    // webpack: {
+    //   devtool: 'inline-source-map',
+    //   modulesDirectories: ['web_modules', 'node_modules', '../app'],
+    //   module: {
+    //     loaders: [
+    //       { test: /\.js?$/, loaders: ['babel-loader?experimental' ], exclude: /node_modules/},
+    //       { test: /\.html$/, loader: "raw"}
+    //     ]
+    //   }
+    // },
+    webpack: require('./config/webpack.config'),
     singleRun: true,
-
-    // Concurrency level
-    // how many browser should be started simultanous
     concurrency: Infinity
   })
 }
