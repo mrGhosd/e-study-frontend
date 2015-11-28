9abf03455795c2c3688b77cc1768f9af5dc550f5
vsokoltsov <vforvad@gmail.com>
2015-11-29 01:14:46 +0300

    add notification directive specs

---
+++ specs/notification/notification_message.spec.js
@@ -0,0 +1,23 @@
+describe('notificationMessage', function() {
+  var compile = null;
+  var $scope = null;
+  var directiveElem = null;
+  var element = null;
+  var messageElem = null;
+
+  beforeEach(angular.mock.module('estudy'));
+
+  beforeEach(inject(function($compile, $rootScope){
+      $scope = $rootScope.$new();
+      element = angular.element("<div class=\"notification-bar\"></div>");
+      messageElem = angular.element("<div class=\"notification-message\"><div class=\"header\">Заголовок</div></div>")
+      element.append(messageElem);
+      directiveElem = $compile(element)($scope);
+      $scope.$digest();
+    })
+  );
+
+  it ("doesn't equal undefined", function() {
+    expect(messageElem.html()).not.toEqual("");
+  });
+});
