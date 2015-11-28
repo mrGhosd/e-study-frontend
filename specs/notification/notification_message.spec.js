69226befd76da2200528d88cb7e65dbcc40ccd4c
vsokoltsov <vforvad@gmail.com>
2015-11-29 01:57:30 +0300

    add green spec for close cross action in notification message directive

--- specs/notification/notification_message.spec.js
+++ specs/notification/notification_message.spec.js
@@ -4,6 +4,7 @@ describe('notificationMessage', function() {
   var directiveElem = null;
   var element = null;
   var messageElem = null;
+  var compileMessage = null;
 
   beforeEach(angular.mock.module('estudy'));
 
@@ -19,5 +20,12 @@ describe('notificationMessage', function() {
 
   it ("doesn't equal undefined", function() {
     expect(messageElem.html()).not.toEqual("");
+  });
+
+  it ("close message after click on cross", function() {
+    var cross = directiveElem.find("a");
+    cross.triggerHandler('click');
+    $scope.$digest();
+    expect(directiveElem.html()).toEqual("");
   });
 });
