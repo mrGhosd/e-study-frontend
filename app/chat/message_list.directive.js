34135310a8b3feed9166b2b0560c503ca705285a
vsokoltsov <vforvad@gmail.com>
2015-12-13 18:46:57 +0300

    add scrolling to top of messages list

--- app/chat/message_list.directive.js
+++ app/chat/message_list.directive.js
@@ -10,38 +10,38 @@ export default function messageListDirective() {
     scope: true,
     bindToController: true,
     controller: MessageListController,
-    controllerAs: "ctrl"
-    // link: function($scope, element) {
-    //   var alreadyAtBottom = true;
-    //
-    //   function scrollToBottom() {
-    //     if (alreadyAtBottom) {
-    //       element.scrollTop(element.prop("scrollHeight"));
-    //     }
-    //   }
-    //
-    //   function isAtBottom() {
-    //     var scrollTop = element.scrollTop();
-    //     var maxHeight = element.prop("scrollHeight") - element.prop("clientHeight");
-    //
-    //     return scrollTop >= maxHeight;
-    //   }
-    //
-    //   // https://developer.mozilla.org/en/docs/Web/API/MutationObserver
-    //   var observer = new window.MutationObserver(scrollToBottom);
-    //
-    //   observer.observe(element[0], { childList: true });
-    //
-    //   var throttledOnScrollHandler = _.throttle(function() {
-    //     alreadyAtBottom = isAtBottom();
-    //   }, 250);
-    //
-    //   element.on("scroll", throttledOnScrollHandler);
-    //
-    //   $scope.$on("$destroy", () => {
-    //     element.off("scroll", throttledOnScrollHandler);
-    //     observer.disconnect();
-    //   });
-    // }
+    controllerAs: "ctrl",
+    link: function($scope, element) {
+      var alreadyAtBottom = true;
+
+      function scrollToBottom() {
+        if (alreadyAtBottom) {
+          $(element).scrollTop(element.prop("scrollHeight"));
+        }
+      }
+
+      function isAtBottom() {
+        var scrollTop = $(element).scrollTop();
+        var maxHeight = element.prop("scrollHeight") - element.prop("clientHeight");
+
+        return scrollTop >= maxHeight;
+      }
+
+      // https://developer.mozilla.org/en/docs/Web/API/MutationObserver
+      var observer = new window.MutationObserver(scrollToBottom);
+
+      observer.observe(element[0], { childList: true });
+
+      var throttledOnScrollHandler = _.throttle(function() {
+        alreadyAtBottom = isAtBottom();
+      }, 250);
+
+      element.on("scroll", throttledOnScrollHandler);
+
+      $scope.$on("$destroy", () => {
+        element.off("scroll", throttledOnScrollHandler);
+        observer.disconnect();
+      });
+    }
   };
 }
