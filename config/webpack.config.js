1e23d19b473588628932d7b26003c24cafd33432
Vadim Sokoltsov <vforvad@gmail.com>
2016-02-25 00:46:59 +0300

    improve some webpack configs

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,6 +2,7 @@ var webpack = require('webpack');
 var pkg = require('../package.json');
 var util = require('util');
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var DEV = process.env.NODE_ENV === 'development';
 var jsBundle = path.join('js', util.format('bundle.js'));
 
@@ -27,11 +28,11 @@ module.exports = {
     filename: 'bundle.js'
   },
   plugins: [
-      // new webpack.ProvidePlugin({
-      //     $: "jquery",
-      //     jQuery: "jquery",
-      // }),
-      // new ExtractTextPlugin("bootstrap.css"),
+      new webpack.ProvidePlugin({
+          $: "jquery",
+          jQuery: "jquery",
+      }),
+      new ExtractTextPlugin("bootstrap.css"),
       new webpack.HotModuleReplacementPlugin(),
       // new webpack.NoErrorsPlugin(),
       // new WebPackAngularTranslate.Plugin(),
@@ -44,12 +45,12 @@ module.exports = {
         { test: /\.html$/, loader: "raw"},
         { test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/, loader: 'file'},
         { test: /\.scss$/, loaders: ["style", "css", "sass"] },
-        // { test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
-        // { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css?sourceMap!postcss')},
-        // { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&mimetype=application/font-woff" },
-        // { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=application/octet-stream" },
-        // { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
-        // { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/svg+xml" },
+        { test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
+        { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css?sourceMap!postcss')},
+        { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&mimetype=application/font-woff" },
+        { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=application/octet-stream" },
+        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
+        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/svg+xml" },
         { test: /\.json$/, exclude: /node_modules/, loaders: ['json-loader'] }
       ]
   },
