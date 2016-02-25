bafe2653d5232392792e4be0d70ccc08cda2cd9f
Vadim Sokoltsov <vforvad@gmail.com>
2016-02-25 21:33:16 +0300

    add some fixes to popup_message

--- app/run_configuration/current_user.js
+++ app/run_configuration/current_user.js
@@ -1,9 +1,14 @@
 export function currentUser($rootScope, WebSockets, PopupMessage, AuthService) {
+  let queue = [];
   $rootScope.$on('currentUser', (event, args) => {
     WebSockets.on(`user${args.user.id}chatmessage`, (event, data) => {
       const message = angular.fromJson(data.obj);
-      if (message.user.id !== AuthService.currentUserValue.id){
-           PopupMessage.postMessage(message);
+      if (!queue.map(message => message.id).includes(message.id)) {
+          queue.push(message);
+
+          if (message.user.id !== AuthService.currentUserValue.id){
+               PopupMessage.postMessage(message);
+          }
       }
     });
   });
