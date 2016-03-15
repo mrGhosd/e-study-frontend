0f6a8240ced0916d4c648dc24ebbbc9e6f95e863
Vadim Sokoltsov <vforvad@gmail.com>
2016-03-15 23:53:03 +0300

    change md-toolbar color

--- app/app.js
+++ app/app.js
@@ -25,7 +25,7 @@ import 'babel-core/polyfill';
 import __UtilPolyfill from 'util/polyfill';
 import angularElastic from 'angular-elastic';
 import './index.html';
-// import 'css/main.scss';
+import 'css/main.scss';
 import 'font-awesome/scss/font-awesome.scss';
 import 'font-awesome/fonts/FontAwesome.otf';
 import 'angular-material/angular-material.scss';
