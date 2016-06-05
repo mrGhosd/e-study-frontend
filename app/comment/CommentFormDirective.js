0acbd6a46c8888d1f9b2b8dcdcbd29a9950ac4cc
Vadim Sokoltsov <vforvad@gmail.com>
2016-06-05 22:26:41 +0300

    add png icons for edit/remove action + implement updating of comment

--- app/comment/CommentFormDirective.js
+++ app/comment/CommentFormDirective.js
@@ -1,8 +1,8 @@
 import template from './comment_form.html';
 
-commentsFormDirective.$inject = ['CommentFactory'];
+commentsFormDirective.$inject = ['CommentFactory', '$location'];
 
-export default function commentsFormDirective(CommentFactory) {
+export default function commentsFormDirective(CommentFactory, $location) {
   return {
     restrict: "E",
     template: template,
@@ -10,6 +10,7 @@ export default function commentsFormDirective(CommentFactory) {
     scope: false,
     link: function($scope, element, attrs) {
       $scope.commentText = '';
+      $scope.formComment = {};
 
       $scope.createComment = function() {
         createComment();
@@ -20,24 +21,39 @@ export default function commentsFormDirective(CommentFactory) {
       }
 
       function createComment() {
+        let promise = {};
         const params = {
           text: $scope.commentText,
           id: $scope.course.id,
           type: 'course'
         };
 
-        CommentFactory.create(params)
-        .then((comment) => {
-          $scope.comments.push(comment);
-          $scope.commentText = '';
-        })
-        .catch((error) => {
-          $scope.commentForm.$submitted = true;
-          $scope.commentForm.$errors = error;
-          $scope.commentForm.$invalid = true;
-          console.log($scope.commentForm);
-          // console.log(error);
-        });
+        if ($scope.formComment.id) {
+          // promise =
+          $scope.formComment.text = $scope.commentText;
+          let ids = $scope.comments.map(v => v.id);
+          let index = ids.indexOf($scope.formComment.id);
+          $scope.comments.splice(index, 1, $scope.formComment)
+          if ($location.hash() !== $scope.formComment.id ) {
+              $location.hash($scope.formComment.id);
+          }
+          $scope.commentText = null;
+        }
+        else {
+          // promise = CommentFactory.create(params)
+        }
+
+        // promise
+        // .then((comment) => {
+        //   $scope.comments.push(comment);
+        //   $scope.commentText = '';
+        // })
+        // .catch((error) => {
+        //   $scope.commentForm.$submitted = true;
+        //   $scope.commentForm.$errors = error;
+        //   $scope.commentForm.$invalid = true;
+        //   console.log($scope.commentForm);
+        // });
       }
     }
   };
--- app/comment/CommentsListDirective.js
+++ app/comment/CommentsListDirective.js
@@ -1,15 +1,27 @@
 import template from './comments_list.html';
 
-commentsListDirective.$inject = [];
+commentsListDirective.$inject = ['$anchorScroll', '$location'];
 
-export default function commentsListDirective() {
+export default function commentsListDirective($anchorScroll, $location) {
   return {
     restrict: "E",
     template: template,
     replace: true,
     scope: false,
     link: function($scope, element, attrs) {
+      $scope.commentFormVisible = false;
 
+      $scope.editComment = function(comment) {
+        $scope.formComment = comment;
+        $scope.commentText = comment.text;
+        if ($location.hash() !== 'commentForm' ) {
+            $location.hash('commentForm');
+        }
+        else {
+          $anchorScroll();
+        }
+
+      }
     }
   };
 }
