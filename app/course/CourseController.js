7a3de0346ac26504ebcabcfe2ca2a45df7d69c31
Vadim Sokoltsov <vforvad@gmail.com>
2016-06-13 00:16:32 +0300

    remove useless actions from course_controller

--- app/course/CourseController.js
+++ app/course/CourseController.js
@@ -8,17 +8,4 @@ export default class CourseController {
     $scope.object = course;
     $scope.type = 'course';
   }
-
-  deleteHomework(homework) {
-    this.HomeworkFactory.delete(this.course.id, homework.id).then((response) => {
-      this.removeFromList(homework);
-    });
-  }
-
-  removeFromList(homework) {
-    if (this.course.homeworks.includes(homework)) {
-      let index = this.course.homeworks.indexOf(homework);
-      this.course.homeworks.splice(index, 1);
-    }
-  }
 }
