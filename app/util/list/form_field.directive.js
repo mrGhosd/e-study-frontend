b00535aba6867631e51ddcffd8b0453cd6dbc760
Vadim Sokoltsov <vforvad@gmail.com>
2016-06-13 00:53:48 +0300

    implement simple form-field directive

---
+++ app/util/list/form_field.directive.js
@@ -0,0 +1,23 @@
+import angular from 'angular';
+import template from './form_field.html';
+
+formFieldDirective.$inject = ['$translate'];
+
+export default function formFieldDirective($translate) {
+  return {
+    restrict: "E",
+    template: template,
+    transclude:true,
+    scope: {
+      title: '=',
+      form: '=',
+      field: '='
+    },
+    link: function($scope, element, attr) {
+      console.log($scope.title);
+      $translate($scope.title).then((translated) => {
+          $scope.translatedTitle = translated;
+      });
+    }
+  };
+}
--- app/util/list/index.js
+++ app/util/list/index.js
@@ -1,7 +1,9 @@
 import angular from 'angular';
 
 import listDirective from './list.directive.js';
+import formFieldDirective from './form_field.directive.js';
 
-export default angular.module('estudy.utils.list', [])
+export default angular.module('estudy.utils', [])
       .directive('list', listDirective)
+      .directive('formField', formFieldDirective)
       .name;
