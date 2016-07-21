9faf479d0fd97db7fadf63f11270be8537736d96
Vadim Sokoltsov <vforvad@gmail.com>
2016-07-22 01:02:47 +0300

    display user information on course page

--- app/course/course.model.js
+++ app/course/course.model.js
@@ -1,3 +1,5 @@
+import User from 'users/user.model.js';
+
 export default class Course {
   constructor(attributes){
       this.setAttributes(attributes);
@@ -7,10 +9,12 @@ export default class Course {
       for(let attr in attributes){
           this[attr] = attributes[attr];
       }
+      if (attributes.author) {
+        this.author = new User(attributes.author);
+      }
   }
 
   avatarURL() {
-
       if (this.image) {
         return this.image.url;
       }
