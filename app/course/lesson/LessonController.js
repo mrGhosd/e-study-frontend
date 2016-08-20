61441026e1e0b61619289117d7b8264ad96d73a9
Vadim Sokoltsov <vforvad@gmail.com>
2016-08-20 23:59:56 +0300

    add handling currentUser on lesson page

--- app/course/lesson/LessonController.js
+++ app/course/lesson/LessonController.js
@@ -1,5 +1,6 @@
 export default class LessonController {
-  constructor($scope, lesson, HomeworkFactory, AuthService, currentUserFactory) {
+  constructor($scope, lesson, HomeworkFactory, AuthService, currentUserFactory,
+              $state, $rootScope) {
     this.lesson = lesson;
     this.$scope = $scope;
     $scope.lesson = lesson;
@@ -8,7 +9,11 @@ export default class LessonController {
     $scope.type = 'lesson';
     $scope.courseId = lesson.course.slug || lesson.course.id;
     this.HomeworkFactory = HomeworkFactory;
+    this.currentUserFactory = currentUserFactory;
+    this.$state = $state;
+    this.$rootScope = $rootScope;
     $scope.currentUser = currentUserFactory.getUser();
+    this.handleCurrentUser();
   }
 
   deleteHomework(homework) {
@@ -22,5 +27,22 @@ export default class LessonController {
       let index = this.lesson.homeworks.indexOf(homework);
       this.lesson.homeworks.splice(index, 1);
     }
+  }
+
+  handleCurrentUser() {
+    let self = this;
+    this.$rootScope.$on('currentUser', (event, args) => {
+      this.currentUserFactory.setUser(args.user);
+      this.currentUser = this.currentUserFactory.getUser();
+      this.$state.go('lesson', { course_id: this.lesson.course.slug || this.lesson.course.id,
+                                 id: this.lesson.slug || this.lesson.id });
+    });
+    this.$rootScope.$on('signedOut', (event, args) => {
+        const defaultUser = {
+          studying_courses: []
+        };
+        this.currentUserFactory.setUser(defaultUser);
+        this.$state.go('course', { id: this.lesson.course.slug || this.lesson.course.id });
+    });
   }
 }
