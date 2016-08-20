0760d5e2ed5e1a8e551700189b041960cf3d70b8
Vadim Sokoltsov <vforvad@gmail.com>
2016-08-21 00:13:39 +0300

    implement handling current user on homework page

--- app/course/homework/HomeworkController.js
+++ app/course/homework/HomeworkController.js
@@ -1,10 +1,29 @@
 export default class HomeworkController {
-  constructor($scope, homework) {
+  constructor($scope, homework, $rootScope, currentUserFactory) {
     $scope.homework = homework;
     $scope.object = homework;
     $scope.type = 'homework';
     $scope.comments = homework.comments;
     $scope.courseId = homework.course.slug || homework.course.id;
     $scope.lessonId = homework.lesson.slug || homework.lesson.id;
+    this.$rootScope = $rootScope;
+    this.currentUserFactory = currentUserFactory;
+  }
+
+  handleCurrentUser() {
+    let self = this;
+    this.$rootScope.$on('currentUser', (event, args) => {
+      this.currentUserFactory.setUser(args.user);
+      this.$scope.currentUser= this.currentUserFactory.getUser();
+      this.emptyInfo = this.emptyUserInfo();
+    });
+    this.$rootScope.$on('signedOut', (event, args) => {
+        const defaultUser = {
+          studying_courses: []
+        };
+        this.currentUserFactory.setUser(defaultUser);
+        this.emptyInfo = this.emptyUserInfo();
+        this.$state.go('course', { id: this.homework.course.slug || this.homework.course.id });
+    });
   }
 }
