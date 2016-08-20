646cd34d9c57aadd1c11613885add8f503d11794
Vadim Sokoltsov <vforvad@gmail.com>
2016-08-20 23:51:09 +0300

    add current user handler for courses list page

--- app/course/CoursesController.js
+++ app/course/CoursesController.js
@@ -1,8 +1,12 @@
 export default class CoursesController {
-  constructor($scope, courses, CourseFactory, currentUserFactory) {
+  constructor($scope, courses, CourseFactory, currentUserFactory, $rootScope) {
     this.courses = courses;
     this.CourseFactory = CourseFactory;
+    this.currentUserFactory = currentUserFactory;
     this.currentUser = currentUserFactory.getUser();
+    this.emptyInfo = this.emptyUserInfo();
+    this.$rootScope = $rootScope;
+    this.handleCurrentUser();
   }
 
   destroy(course) {
@@ -17,5 +21,27 @@ export default class CoursesController {
       let index = this.courses.indexOf(course);
       this.courses.splice(index, 1);
     }
+  }
+
+  emptyUserInfo() {
+    const keys = Object.keys(this.currentUser);
+    return keys.length === 1, keys.first === 'studying_courses';
+  }
+
+  handleCurrentUser() {
+    let self = this;
+    this.$rootScope.$on('currentUser', (event, args) => {
+      this.currentUserFactory.setUser(args.user);
+      this.currentUser = this.currentUserFactory.getUser();
+      this.emptyInfo = this.emptyUserInfo();
+    });
+    this.$rootScope.$on('signedOut', (event, args) => {
+        const defaultUser = {
+          studying_courses: []
+        };
+        this.currentUserFactory.setUser(defaultUser);
+        this.currentUser = this.currentUserFactory.getUser();
+        this.emptyInfo = this.emptyUserInfo();
+    });
   }
 }
